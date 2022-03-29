42cd7130cf97ed624a9035a8d5d085caa807c1ab
HRNV <101626022+HRNV@users.noreply.github.com>
2022-03-29 15:11:14 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+# read the input image
+img = cv2.imread("CR7.jpg")
+# convert from BGR to RGB so we can plot using matplotlib
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# disable x & y axis
+plt.axis('off')
+# show the image
+plt.imshow(img)
+plt.show()
+
+# get the image shape
+rows, cols, dim = img.shape
+print(rows)
+print(cols)
+# transformation matrix for translation
+M = np.float32([[1, 0, 500],
+                [0, 1, 500],
+                [0, 0, 1]])
+# apply a perspective transformation to the image
+translated_img = cv2.warpPerspective(img, M, (cols, rows))
+# disable x & y axis
+plt.axis('off')
+# show the resulting image
+plt.imshow(translated_img)
+plt.show()
+# save the resulting image to disk
+plt.imsave("CR7_translated.jpg", translated_img)
