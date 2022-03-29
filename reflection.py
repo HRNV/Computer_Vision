cbfcdc9a05feb15974345d80a6c814496ad8dd4c
HRNV <101626022+HRNV@users.noreply.github.com>
2022-03-29 15:14:59 +0530

    Update reflection.py

--- reflection.py
+++ reflection.py
@@ -3,32 +3,36 @@ import cv2
 import matplotlib.pyplot as plt
 
 # read the input image
-img = cv2.imread("CR7.jpg")
-# convert from BGR to RGB so we can plot using matplotlib
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-# disable x & y axis
+imag = cv2.imread("CR7.jpg")
+#convert BGR to RGB
+imag = cv2.cvtColor(imag, cv2.COLOR_BGR2RGB)
+
+
+# switch off the axis
 plt.axis('off')
 # show the image
-plt.imshow(img)
+plt.imshow(imag)
 plt.show()
 
-# get the image shape
-rows, cols, dim = img.shape
-
-# transformation matrix for x-axis reflection 
-M = np.float32([[1,  0, 0   ],
-                [0, -1, rows],
+# the dimensions. 
+r, c, dim = img.shape
+# for x axis
+Mr = np.float32([[1,  0, 0   ],
+                [0, -1, r],
                 [0,  0, 1   ]])
-# transformation matrix for y-axis reflection
-# M = np.float32([[-1, 0, cols],
+# for y-axis reflection
+# Mr = np.float32([[-1, 0, c],
 #                 [ 0, 1, 0   ],
 #                 [ 0, 0, 1   ]])
 # apply a perspective transformation to the image
-reflected_img = cv2.warpPerspective(img,M,(int(cols),int(rows)))
-# disable x & y axis
+reflected_imag = cv2.warpPerspective(imag,Mr,(int(c),int(r)))
+
+# switch off
 plt.axis('off')
+
 # show the resulting image
 plt.imshow(reflected_img)
 plt.show()
+
 # save the resulting image to disk
 plt.imsave("CR7.jpg", reflected_img)
